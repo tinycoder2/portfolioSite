4efaad7feb2bde42c9588f856eda65c950c969fa
Swaathy <swaathykumar02@gmail.com>
2020-10-13 19:13:38 +0530

    made navbar dissapear

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(function () {
-    var roles = ["Artist", "Dancer", "Student"];
-    var count = 0;
-    setInterval(() => {
-        $("#intro .content h2 span").fadeOut("fast", function () {
-         $(this).text(roles[count++ % roles.length]);
-         $(this).fadeIn("fast");
-        });
-    }, 2500);
-    });
+  var roles = ["Artist", "Dancer", "Student"];
+  var count = 0;
+  /*var $typeSpan = $("#intro .content h2 span").typist*/
+
+  setInterval(() => {
+      $("#intro .content h2 span").fadeOut("fast", function () {
+       $(this).text(roles[count++ % roles.length]);
+       $(this).fadeIn("fast");
+      });
+  }, 2500);
+  $(".navbar").hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 40) {
+      $(".navbar").slideDown(100);
+    } else {
+      $(".navbar").slideUp(100);
+    }
+  });
+});
