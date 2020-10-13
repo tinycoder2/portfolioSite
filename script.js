30fba53c900026b1282692ea98b798d910e2a840
Swaathy <swaathykumar02@gmail.com>
2020-10-13 19:58:44 +0530

    typist effect

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $(function () {
   var roles = ["Artist", "Dancer", "Student"];
   var count = 0;
-  /*var $typeSpan = $("#intro .content h2 span").typist*/
-
+  var $typeSpan = $("#intro .content h2 span")
+  .typist({
+    text: roles[count]
+  });
   setInterval(() => {
-      $("#intro .content h2 span").fadeOut("fast", function () {
-       $(this).text(roles[count++ % roles.length]);
-       $(this).fadeIn("fast");
-      });
-  }, 2500);
+      //$("#intro .content h2 span").fadeOut("fast", function () {
+      // $(this).text(roles[count++ % roles.length]);
+      // $(this).fadeIn("fast");
+      //});
+
+    $typeSpan
+    .typistRemove(roles[count++ % roles.length].length)
+    .typistPause(2000)
+    .typistAdd(roles[count % roles.length]);
+  }, 5000);
+
+  //$("#intro .content h2 span").typist({
+    //text: "Artist"
+  //})
+
   $(".navbar").hide();
   $(window).scroll(function () {
     if ($(window).scrollTop() > 40) {
